7031e123daed07ec450086d893ecd4026f28c840
Patricio Abarca <patricioabarca88@gmail.com>
2024-05-07 15:44:56 -0400

    se crea estructura

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NavBarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
